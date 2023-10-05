03a9ae8ed91b31f60f72bb598ba620f7c106c842
Rahul Chavan <105979644+rahulcdev13@users.noreply.github.com>
2023-10-06 00:40:13 +0530

    updated code with microfront end part and

---
+++ CartModule_UI/src/index.js
@@ -0,0 +1,5 @@
+// import faker from "@faker-js/faker"
+import { faker } from "@faker-js/faker";
+
+let cartList = `<div>You have ${faker.datatype.number(20)} items your cart</div>`;
+document.querySelector('#cart-list').innerHTML = cartList;
---
+++ ProductModule_UI/src/index.js
@@ -0,0 +1,9 @@
+// import faker from "@faker-js/faker"
+import { faker } from "@faker-js/faker";
+
+let productList = '';
+
+for (let i = 0; i < 10; i++) {
+    productList += `<div>${faker.commerce.productName()}</div>`   
+}
+document.querySelector('#product-list').innerHTML = productList;
