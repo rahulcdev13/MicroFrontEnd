15da9615ff92416cf8fa3fec872bb85e929d84f8
Rahul Chavan <105979644+rahulcdev13@users.noreply.github.com>
2023-10-20 01:29:03 +0530

    ssdfsdf

--- CartModule_UI/src/index.js
+++ CartModule_UI/src/index.js
@@ -1,5 +1,9 @@
 // import faker from "@faker-js/faker"
 import { faker } from "@faker-js/faker";
 
+<<<<<<< HEAD
+let cartList = `<div>You have Rahul samat ${faker.datatype.number(20)} items your cart</div>`;
+=======
 let cartList = `<div>You have Rahul ${faker.datatype.number(20)} items your cart</div>`;
+>>>>>>> 41e576f1c9657dd7c7b61a1fddc6c3f57fdf2be6
 document.querySelector('#cart-list').innerHTML = cartList;
